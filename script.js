298f6205cdbdf1be895b688c289e382bf24d5bf9
João Paulo <joaopaulojpsp@gmail.com>
2022-02-19 09:50:55 -0300

    Atualização no javascript e css

---
+++ script.js
@@ -0,0 +1,13 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+// 0 - verde
+// 1 - vermelho
+// 2 - amarelo
+// 3 - azul
+
+const blue = document.querySelector(".blue");
+const red = document.querySelector(".red");
+const yellow = document.querySelector(".yellow");
+const green = document.querySelector(".green");
