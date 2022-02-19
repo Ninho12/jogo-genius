bcd86bb0084c27cc167f465464606277f0d155ff
João Paulo <joaopaulojpsp@gmail.com>
2022-02-19 10:11:53 -0300

    Atualização no js

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ const blue = document.querySelector(".blue");
 const red = document.querySelector(".red");
 const yellow = document.querySelector(".yellow");
 const green = document.querySelector(".green");
+
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4)
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order){
+        let elementColor = createElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(()=>{
+
+        element.classList.add('selected');
+
+    }, number - 250);
+
+    setTimeout(()=>{
+        element.classList.remove('selected');
+    })
+}
+
+
+let checkOrder = () => {
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            lose();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length){
+        alert('Pontuação: ${score} \n Você acertou! Iniciando proximo nivel!');
+        nextLevel();
+    }
+
+}
+
+
